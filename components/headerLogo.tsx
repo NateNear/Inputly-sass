78824ce2f9492fa16172787419731b2adc11e019
Harindra.Reddy <harindra.reddy@matter.in>
2025-01-15 17:52:49 +0530

    refactor: Add navigation functionality to header logo component

--- components/headerLogo.tsx
+++ components/headerLogo.tsx
@@ -5,17 +5,25 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 import lightModeLogo from '../public/logo-lightMode.png'
 import darkModeLogo from '../public/logo-darkMode.png'
+import { useRouter } from "next/navigation";
 
 function HeaderLogo() {
   const { theme } = useTheme();
 
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push(`/`);
+  };
+
   return (
     <Image
-      className="flex justify-start flex-shrink pl-1 w-16 lg:w-[110px] pb-[5px] md:pl-4 lg:pl-6"
+      className="flex justify-start flex-shrink pl-1 w-16 lg:w-[110px] pb-[5px] md:pl-4 lg:pl-6 hover:cursor-pointer"
       src={theme === "dark" ? darkModeLogo : lightModeLogo}
       width={110}
       height={110}
       alt="logo"
+      onClick={handleNavigation}
     />
   );
 }
