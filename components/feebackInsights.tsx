522ebcfb0bd4d5fd5ea29d1e3c6a4bada1214782
Harindra.Reddy <harindra.reddy@matter.in>
2025-01-21 23:17:57 +0530

    maybe

--- components/feebackInsights.tsx
+++ components/feebackInsights.tsx
@@ -1,14 +1,14 @@
-import { getFeedbacks } from '@/index';
-import FeedbackAnalysisClient from './feedbackAnalysisComponent';
+// import { getFeedbacks } from '@/index';
+// import FeedbackAnalysisClient from './feedbackAnalysisComponent';
 
-interface FeedbackInsightsProps {
-  projectId: number;
-}
+// interface FeedbackInsightsProps {
+//   projectId: number;
+// }
 
-export default async function FeedbackInsights({ projectId }: FeedbackInsightsProps) {
+// export default async function FeedbackInsights({ projectId }: FeedbackInsightsProps) {
   
-  const feedbacks = await getFeedbacks(projectId);
-  const first10Feedbacks = feedbacks.slice(0, 10);
+//   const feedbacks = await getFeedbacks(projectId);
+//   const first10Feedbacks = feedbacks.slice(0, 10);
   
-  return <FeedbackAnalysisClient initialFeedbacks={first10Feedbacks} />;
-}
+//   return <FeedbackAnalysisClient initialFeedbacks={first10Feedbacks} />;
+// }
