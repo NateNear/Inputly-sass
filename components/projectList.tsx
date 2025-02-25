4b564a16f0ea94d5c3c97453e1d33ce7b04788c9
Harindra.Reddy <harindra.reddy@matter.in>
2025-02-25 15:06:50 +0530

    solved "go to project" button not clickable error

--- components/projectList.tsx
+++ components/projectList.tsx
@@ -57,7 +57,6 @@ export async function ProjectList() {
               <DeleteProjectButton projectId={project.id} />
             </div>
             
-            <div className="absolute inset-0 bg-gradient-to-br from-purple-500/5 to-indigo-500/5 opacity-0 group-hover:opacity-100 transition-opacity" />
             
             <CardHeader className="relative">
               <div className="flex items-center space-x-2">
